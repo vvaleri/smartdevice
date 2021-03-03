7f4596656f858d7e6f444398a57f67e20ad61f1f
Valeria Rodionova <valeriya_off@mail.ru>
2021-03-03 10:28:12 +0300

    Исправляет отображение треугольников главного экрана

--- source/js/modules/open-accordion.js
+++ source/js/modules/open-accordion.js
@@ -1,43 +1,50 @@
 const accordionBtn = document.querySelectorAll('.accordion__btn');
 const accordionContent = document.querySelectorAll('.accordion__content');
-const content = document.querySelector('.footer__content');
-const breakpointMobile = window.matchMedia('(max-width:767px)');
+// const content = document.querySelector('.footer__content');
+// const title = document.querySelectorAll('.accordion__title');
+// const breakpointMobile = window.matchMedia('(max-width:767px)');
 
 const openAccordion = () => {
 
-  const openMenu = (i) => {
-    accordionContent[i].classList.toggle('active');
-    accordionBtn[i].classList.toggle('active');
-  };
+  // const openMenu = (i) => {
+  //   accordionContent[i].classList.toggle('active');
+  //   accordionBtn[i].classList.toggle('active');
+  // };
 
-  const closeMenu = (i) => {
-    accordionContent[i].classList.remove('active');
-    accordionBtn[i].classList.remove('active');
-  };
+  // const closeMenu = (i) => {
+  //   accordionContent[i].classList.remove('active');
+  //   accordionBtn[i].classList.remove('active');
+  // };
+
+  accordionBtn.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      accordionContent[i].classList.toggle('active');
+      accordionBtn[i].classList.toggle('active');
+    });
+  });
 
 
-  const initAccordion = (e) => {
-    if (e.target.classList.contains('accordion__btn')) {
-      accordionBtn.forEach((item, i) => {
-        if (e.target === item || e.code === 'Enter') {
-          openMenu(i);
-        } else {
-          closeMenu(i);
-        }
-      });
-    }
-  };
+  // const initAccordion = (e) => {
+  //   if (e.target.classList.contains('accordion__btn')) {
+  //     accordionBtn.forEach((item, i) => {
+  //       if (e.target === item) {
+  //         openMenu(i);
+  //       } else {
+  //         closeMenu(i);
+  //       }
+  //     });
+  //   }
+  // };
 
-  const isMobile = () => {
-    if (breakpointMobile.matches) {
-      content.addEventListener('click', initAccordion);
-      content.addEventListener('keyup', initAccordion);
-    }
-  };
+  // const isMobile = () => {
+  //   if (breakpointMobile.matches) {
+  // content.addEventListener('click', initAccordion);
+  //   }
+  // };
 
-  isMobile();
+  // isMobile();
 
-  breakpointMobile.addEventListener('change', isMobile);
+  // breakpointMobile.addEventListener('change', isMobile);
 };
 
 export {openAccordion};
